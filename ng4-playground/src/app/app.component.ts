e06401ad84dc05786f6313d87333d2833c01bf2e
kyledobash <kyledobash@yahoo.com>
2021-03-30 10:19:47 -0700

    disabled property bound to button

--- ng4-playground/src/app/app.component.ts
+++ ng4-playground/src/app/app.component.ts
@@ -5,14 +5,12 @@ import { Component } from '@angular/core';
   template: `
   <h1>Hey guys!</h1>
 
-  <img src="{{angularLogo}}">
-  <img [src]="angularLogo">
-  <img bind-src="angularLogo">
+  <button [disabled]="buttonStatus">My Button</button>
 
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  angularLogo = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fcf%2FAngular_full_color_logo.svg%2F1200px-Angular_full_color_logo.svg.png&f=1&nofb=1';
+  buttonStatus = false;
 }
