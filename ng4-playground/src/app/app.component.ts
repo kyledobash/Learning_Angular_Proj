785df8e783a98f4c701f18f1088cd75e9cffe60c
kyledobash <kyledobash@yahoo.com>
2021-03-30 12:45:35 -0700

    implemented class binding through property binding manipulating css

--- ng4-playground/src/app/app.component.ts
+++ ng4-playground/src/app/app.component.ts
@@ -3,15 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Hello!</h1>
+  <h1 [class]="titleClass">Hello!</h1>
   `,
   styles: [`
   h1 {
     text-decoration: underline;
   }
+
+  .red-title {
+    color: red;
+  }
   `]
 })
 export class AppComponent {
 
+  titleClass = 'red-title'
 
 }
